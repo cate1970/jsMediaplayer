a49fd5af130a2d0f4cd9d512bd36ce9d7feb8b2e
TERASAC <carlos.teran.gutierrez@gmail.com>
2022-07-06 15:14:10 -0500

    update 1

--- src/index.js
+++ src/index.js
@@ -3,4 +3,20 @@
  * When you're ready to start on your site, clear the file. Happy hacking!
  **/
 
+ let listVideo = document.querySelectorAll(".video-list .vid");
+ let mainVideo = document.querySelector(".main-video video");
+ let title = document.querySelector(".main-video .title");
+ listVideo.forEach((video) => {
+   video.onclick = () => {
+     listVideo.forEach((vid) => vid.classList.remove("active"));
+     video.classList.add("active");
+     if (video.classList.contains("active")) {
+       let src = video.children[0].getAttribute("src");
+       mainVideo.src = src;
+       let text = video.children[1].innerHTML;
+       title.innerHTML = text;
+     }
+   };
+ });
+ 
 console.log('Happy hacking :)')
